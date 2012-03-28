c3f7ee3890b3216994dad838e57dad5eb707a851
Ethan Steinman <ersteinm@ncsu.edu>
2012-03-28 13:57:21 -0400

    Initial commit.

---
+++ CrowdFarm/src/edu/ncsu/csc/crowdfarm/CrowdFarmActivity.java
@@ -0,0 +1,13 @@
+package edu.ncsu.csc.crowdfarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrowdFarmActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
