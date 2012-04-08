9524d6e034e933b68f6910ed37dc391d9dae10f3
Ethan Steinman <ersteinm@ncsu.edu>
2012-04-08 14:59:04 -0400

    added beans

---
+++ CrowdFarm/src/edu/ncsu/csc/crowdfarm/beans/RegistrationBean.java
@@ -0,0 +1,79 @@
+package edu.ncsu.csc.crowdfarm.beans;
+
+public class RegistrationBean {
+	
+	private String firstName;
+	private String middleInitial;
+	private String lastName;
+	private String email;
+	private String password;
+	private String phone;
+	private String location;
+	private String description;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+	
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
