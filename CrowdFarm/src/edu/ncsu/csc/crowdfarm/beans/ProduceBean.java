6a23528698efb23bc87e3ca5757bb7968f419734
Ethan Steinman <ersteinm@ncsu.edu>
2012-04-08 14:58:52 -0400

    added beans

---
+++ CrowdFarm/src/edu/ncsu/csc/crowdfarm/beans/ProduceBean.java
@@ -0,0 +1,63 @@
+package edu.ncsu.csc.crowdfarm.beans;
+
+public class ProduceBean {
+	
+	private String type;
+	private String amount;
+	private String measure;
+	private String shelfLife;
+	private String lifeUnit;
+	private String cost;
+	
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	public String getMeasure() {
+		return measure;
+	}
+	
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+	
+	public String getShelfLife() {
+		return shelfLife;
+	}
+	
+	public void setShelfLife(String shelfLife) {
+		this.shelfLife = shelfLife;
+	}
+	
+	public String getLifeUnit() {
+		return lifeUnit;
+	}
+	
+	public void setLifeUnit(String lifeUnit) {
+		this.lifeUnit = lifeUnit;
+	}
+	
+	public String getCost() {
+		return cost;
+	}
+	
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+	
+	
+
+}
