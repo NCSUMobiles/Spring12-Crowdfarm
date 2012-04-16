225a2bf39f2368d918b7e097c873d4ca01c1e7b8
Ethan Steinman <ersteinm@ncsu.edu>
2012-04-16 17:38:15 -0400

    reformat register layout with useable ids.  numbered register layouts now out of date.

--- CrowdFarm/src/edu/ncsu/csc/crowdfarm/activities/RegisterActivity.java
+++ CrowdFarm/src/edu/ncsu/csc/crowdfarm/activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package edu.ncsu.csc.crowdfarm.activities;
 
 import edu.ncsu.csc.crowdfarm.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ViewFlipper;
@@ -21,6 +22,15 @@ public class RegisterActivity extends Activity {
 	public void back(View view) {
 		final ViewFlipper regFlipper = (ViewFlipper)findViewById(R.id.registerFlipper);
 		regFlipper.showPrevious();
+	}
+	
+	public void backToLogin(View view) {
+		Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+		RegisterActivity.this.startActivity(loginIntent);
+	}
+	
+	public void submit(View view) {
+		
 	}
 
 }
